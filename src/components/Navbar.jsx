c7931f83789f66cae83ec0b2175b9f2b82f40ebe
RSG-developer <girmajirahul2@gmail.com>
2024-11-26 21:06:34 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,39 @@
 import React, { useState } from "react";
 import "../styles/navbar.css";
-//import { BiMenu } from "react-icons/bi";
+
 function Navbar() {
- 
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
 
   return (
     <div>
-    <div className="scroll-up-btn">
+      <div className="scroll-up-btn">
         <i className="fas fa-angle-up"></i>
-    </div>
-    <nav className="navbar">
+      </div>
+      <nav className="navbar">
         <div className="max-width">
-            <ul className="menu">
-                <li><a href="#home" className="menu-btn">Home</a></li>
-                <li><a href="#about" className="menu-btn">About</a></li>
-                <li><a href="#services" className="menu-btn">Services</a></li>
-                <li><a href="#skills" className="menu-btn">Skills</a></li>
-                <li><a href="#contact" className="menu-btn">Contact</a></li>
-            </ul>
-            
-            <div className="menu-btn">
-                <i className="fas fa-bars"></i>
-            </div>
+          <div className="menu-btn" onClick={toggleMenu}>
+            <i className={`fas ${isMenuOpen ? "fa-times" : "fa-bars"}`}></i>
+          </div>
+          <ul className={`menu ${isMenuOpen ? "active" : ""}`}>
+            <li>
+              <a href="#home" className="menu-btn ">Home</a>
+            </li>
+            <li>
+              <a href="#about" className="menu-btn">About</a>
+            </li>
+            <li>
+              <a href="#services" className="menu-btn">Services</a>
+            </li>
+            <li>
+              <a href="#contact" className="menu-btn">Contact</a>
+            </li>
+          </ul>
         </div>
-    </nav>
+      </nav>
     </div>
   );
 }
